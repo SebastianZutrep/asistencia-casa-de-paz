61c34e21e276be12d33eb13dab3c77e494f7d0d1
Sebastian Pertuz <sebas9991909@gmail.com>
2026-01-14 02:04:04 -0500

    Fix deployment issues: Update Django to 5.1.4 and remove incompatible backports.zoneinfo

--- casa_paz/__init__.py
+++
@@ -1,9 +0,0 @@
-import os
-import cloudinary
-
-# Configurar Cloudinary al inicio
-cloudinary.config(
-    cloud_name=os.environ.get('CLOUDINARY_CLOUD_NAME'),
-    api_key=os.environ.get('CLOUDINARY_API_KEY'),
-    api_secret=os.environ.get('CLOUDINARY_API_SECRET')
-)
